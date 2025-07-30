e5ff2e57aac0a4a226523da181a74ea0a99d5a94
RevainIDN <kov.igor94@gmail.com>
2025-07-30 17:49:35 +0300

    chore: добавлен react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
 import './styles/App.css'
+import { Routes, Route } from 'react-router-dom';
+import { useSpotifyToken } from './hooks/useSpotifyToken';
+import Sidebar from './components/Sidebar/Sidebar';
+import Player from './components/Player/Player';
+import Home from './pages/Home/Home';
+import Search from './pages/Search/Search';
 
 function App() {
+  const { token, isLoading, isError } = useSpotifyToken();
+
+  if (isLoading) return <p>Загрузка токена Spotify...</p>;
+  if (isError || !token) return <p>Ошибка загрузки токена. Проверь консоль.</p>;
 
   return (
-    <>
-
-    </>
+    <div className="spotify-clone">
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<Home token={token} />} />
+        <Route path='/search' element={<Search />} />
+      </Routes>
+      <Player />
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from './App.tsx'
 import store from './store/index.ts'
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </Provider>
 )
