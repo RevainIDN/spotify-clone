7b595a3e9249d7ba13acbd5e30fa48d84f4b833b
RevainIDN <kov.igor94@gmail.com>
2025-07-30 17:43:30 +0300

    feat: добавлено 2 кастомных хука для получения токена spotifyAPI и кеширование данных с помощью ReactQuery

---
+++ src/hooks/usePlaylistsOverview.tsx
@@ -0,0 +1,40 @@
+import { useQueries } from "@tanstack/react-query";
+import playlists from "../services/Playlists/playlists";
+
+export const usePlaylistsOverview = (accessToken: string | null) => {
+    const results = useQueries({
+        queries: [
+            {
+                queryKey: ["newReleases", accessToken],
+                queryFn: () => playlists.getNewReleases(accessToken),
+                staleTime: 5 * 60 * 1000,
+                enabled: !!accessToken,
+            },
+            {
+                queryKey: ["popPlaylists", accessToken],
+                queryFn: () => playlists.getPopPlaylists(accessToken),
+                staleTime: 5 * 60 * 1000,
+                enabled: !!accessToken,
+            },
+            {
+                queryKey: ["rockPlaylists", accessToken],
+                queryFn: () => playlists.getRockPlaylists(accessToken),
+                staleTime: 5 * 60 * 1000,
+                enabled: !!accessToken,
+            },
+            {
+                queryKey: ["relaxPlaylists", accessToken],
+                queryFn: () => playlists.getRelaxPlaylists(accessToken),
+                staleTime: 5 * 60 * 1000,
+                enabled: !!accessToken,
+            }
+        ]
+    });
+
+    return {
+        newReleases: results[0],
+        popPlaylists: results[1],
+        rockPlaylists: results[2],
+        relaxPlaylists: results[3]
+    };
+};
