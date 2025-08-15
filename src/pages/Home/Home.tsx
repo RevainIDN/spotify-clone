9729988ae168d318e520f9c320f41a876975bd62
RevainIDN <kov.igor94@gmail.com>
2025-08-15 18:25:47 +0300

    fix: добавлена загрузка компонентом Loader вместо условной текстовой загрузки

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,6 +4,8 @@ import { usePlaylistsOverview } from '../../hooks/usePlaylistsOverview';
 import { type SimplifiedMappedPlaylistItem } from '../../types/playlists/generalTypes';
 import { mapPlaylistToSimplified } from '../../services/Selections/selections';
 
+import Loader from '../../components/common/Loader';
+
 interface HomeProps {
 	token: string | null;
 }
@@ -11,62 +13,50 @@ interface HomeProps {
 export default function Home({ token }: HomeProps) {
 	const { newReleases, popPlaylists, rockPlaylists, relaxPlaylists } = usePlaylistsOverview(token);
 
+	if (!newReleases || !popPlaylists || !rockPlaylists || !relaxPlaylists) {
+		return <Loader />;
+	}
+
 	return (
 		<div className={home.home}>
-			{newReleases.isLoading ? (
-				<div className={home.skeletonLoader}></div>
-			) : (
-				<PlaylistSection
-					title="New Releases"
-					sectionKey='new-releases'
-					items={newReleases.data?.albums.items || []}
-				/>
-			)}
-			{popPlaylists.isLoading ? (
-				<div className={home.skeletonLoader}></div>
-			) : (
-				<PlaylistSection
-					title="Pop"
-					sectionKey='pop'
-					items={
-						popPlaylists.data
-							? popPlaylists.data.playlists.items
-								.map(mapPlaylistToSimplified)
-								.filter(Boolean) as SimplifiedMappedPlaylistItem[]
-							: []
-					}
-				/>
-			)}
-			{rockPlaylists.isLoading ? (
-				<div className={home.skeletonLoader}></div>
-			) : (
-				<PlaylistSection
-					title="Rock"
-					sectionKey='rock'
-					items={
-						rockPlaylists.data
-							? rockPlaylists.data.playlists.items
-								.map(mapPlaylistToSimplified)
-								.filter(Boolean) as SimplifiedMappedPlaylistItem[]
-							: []
-					}
-				/>
-			)}
-			{relaxPlaylists.isLoading ? (
-				<div className={home.skeletonLoader}></div>
-			) : (
-				<PlaylistSection
-					title="Relax"
-					sectionKey='relax'
-					items={
-						relaxPlaylists.data
-							? relaxPlaylists.data.playlists.items
-								.map(mapPlaylistToSimplified)
-								.filter(Boolean) as SimplifiedMappedPlaylistItem[]
-							: []
-					}
-				/>
-			)}
+			<PlaylistSection
+				title="New Releases"
+				sectionKey='new-releases'
+				items={newReleases.data?.albums.items || []}
+			/>
+			<PlaylistSection
+				title="Pop"
+				sectionKey='pop'
+				items={
+					popPlaylists.data
+						? popPlaylists.data.playlists.items
+							.map(mapPlaylistToSimplified)
+							.filter(Boolean) as SimplifiedMappedPlaylistItem[]
+						: []
+				}
+			/>
+			<PlaylistSection
+				title="Rock"
+				sectionKey='rock'
+				items={
+					rockPlaylists.data
+						? rockPlaylists.data.playlists.items
+							.map(mapPlaylistToSimplified)
+							.filter(Boolean) as SimplifiedMappedPlaylistItem[]
+						: []
+				}
+			/>
+			<PlaylistSection
+				title="Relax"
+				sectionKey='relax'
+				items={
+					relaxPlaylists.data
+						? relaxPlaylists.data.playlists.items
+							.map(mapPlaylistToSimplified)
+							.filter(Boolean) as SimplifiedMappedPlaylistItem[]
+						: []
+				}
+			/>
 		</div>
 	)
 }
