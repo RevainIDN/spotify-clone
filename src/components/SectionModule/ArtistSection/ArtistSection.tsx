42685e8a7dd1696bdcc909c4f06e68a88e10e8d8
RevainIDN <kov.igor94@gmail.com>
2025-09-13 17:23:51 +0300

    feat: реализована секция для отображения артистов

---
+++ src/components/SectionModule/ArtistSection/ArtistSection.tsx
@@ -0,0 +1,46 @@
+import artistSectionStyles from './ArtistSection.module.css'
+import { useNavigate } from 'react-router-dom';
+import { type SimplifiedMappedArtistItem } from '../../../types/collection/generalTypes';
+
+interface ArtistSectionProps {
+	title: string;
+	sectionKey: string;
+	items: SimplifiedMappedArtistItem[];
+}
+
+function ArtistItem({ artist }: { artist: SimplifiedMappedArtistItem }) {
+	const navigate = useNavigate();
+
+	const handleClick = () => {
+		if (artist.type === 'artist') {
+			navigate(`/artist/${artist.id}`);
+			return;
+		}
+	};
+
+	return (
+		<li onClick={handleClick} className={artistSectionStyles.artistItem}>
+			<img className={artistSectionStyles.artistImage} src={artist.images[0]?.url} alt={artist.name} />
+			<span className={artistSectionStyles.artistTitle}>{artist.name}</span>
+			<span className={artistSectionStyles.artistType}>{artist.type && 'Artist'}</span>
+		</li>
+	);
+}
+
+export default function ArtistSection({ title, sectionKey, items }: ArtistSectionProps) {
+	const navigate = useNavigate();
+
+	return (
+		<div className={artistSectionStyles.artistSection}>
+			<div className={artistSectionStyles.header}>
+				<h1 className={artistSectionStyles.title}>{title}</h1>
+				<button onClick={() => navigate(`/section/${sectionKey}`, { state: { title, items } })} className={artistSectionStyles.showAll}>Show all</button>
+			</div>
+			<ul className={artistSectionStyles.artists}>
+				{items.slice(0, 5).map(artist => (
+					<ArtistItem key={artist.id} artist={artist} />
+				))}
+			</ul>
+		</div>
+	);
+}
