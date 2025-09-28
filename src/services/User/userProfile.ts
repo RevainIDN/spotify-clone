5dc692f19005e5852d1029602955ad06b549fbe1
RevainIDN <kov.igor94@gmail.com>
2025-09-28 18:25:58 +0300

    feat: добавлены запросы для получения данных профиля авторизованного пользователя

---
+++ src/services/User/userProfile.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export async function getUserProfileData(token: string) {
+	try {
+		const response = await axios.get(`https://api.spotify.com/v1/me`, {
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		});
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+export async function getUserTopItems(token: string, type: 'artists' | 'tracks') {
+	try {
+		const response = await axios.get(`https://api.spotify.com/v1/me/top/${type}`, {
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		});
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
+}
