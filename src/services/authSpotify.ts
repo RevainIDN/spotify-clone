f5129a676262d753eea26224a689855a193a6d03
RevainIDN <kov.igor94@gmail.com>
2025-09-28 18:27:29 +0300

    feat: добавлен новый скоп для получения прав получения данных spotify api

--- src/services/authSpotify.ts
+++ src/services/authSpotify.ts
@@ -7,6 +7,7 @@ const SCOPES = [
 	'user-read-email',
 	'user-read-private',
 	'user-read-playback-state',
+	'user-top-read',
 	'user-modify-playback-state',
 	'playlist-read-private',
 	'user-library-read',
