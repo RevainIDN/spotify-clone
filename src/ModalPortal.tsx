59b626972012b99f003deb222cd636abb66f5cde
RevainIDN <kov.igor94@gmail.com>
2026-01-09 18:56:36 +0200

    feat: Добавлен ModalPortal для корректного отображения модальных окон

---
+++ src/ModalPortal.tsx
@@ -0,0 +1,8 @@
+import { createPortal } from "react-dom";
+
+export default function ModalPortal({ children }: { children: React.ReactNode }) {
+	const modalRoot = document.getElementById("modal-root");
+	if (!modalRoot) return null;
+
+	return createPortal(children, modalRoot);
+}
